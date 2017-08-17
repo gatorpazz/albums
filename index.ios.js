11a5ec50e80b20d60fe84398ee2660284cfa9201
Michael Pascuzzi <gatorpazz@gmail.com>
2017-08-17 13:33:50 -0400

    First commit

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+// IOS CODE
+
+// Import a library to help create a component
+import React, { Component } from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+// Render it to the device
