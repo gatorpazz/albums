efc9d15ede1270caa784731c2f9d00b4d08a1301
Michael Pascuzzi <gatorpazz@gmail.com>
2017-08-18 16:14:13 -0400

    Added Card section component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => (
-  <View>
-    <Text>{album.title}</Text>
-  </View>
+  <Card>
+    <CardSection>
+      <Text>{album.title}</Text>
+    </CardSection>
+    <CardSection>
+      <Text>{album.title}</Text>
+    </CardSection>
+  </Card>
 );
 
 export default AlbumDetail;
